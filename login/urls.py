2a66b8200b28a76b7ceea88e67061a56e33c8b86
jishnu107 <jleopc1@gmail.com>
2022-08-27 13:34:58 +0530

    added loginpage and homepage heroku login

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loginpage',views.login_page,name='login'),
+    path('',views.login_page,name='login'),
     path('homepage',views.home_page,name='home'),
 ]
